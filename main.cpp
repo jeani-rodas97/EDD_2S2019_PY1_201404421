3800398b21777f0711b768f043723baacd89f2bb
jeani-rodas97 <jenni1897@gmail.com>
2019-08-20 06:40:42 -0600

    Menu inicial

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int seleccion;
+    cout << "%%%%%%%%%%%%%%%%%%%% MENU %%%%%%%%%%%%%%%%%%%%"<< endl;
+    cout << "1. Insert image. "<< endl;
+    cout << "2. Select image. "<< endl;
+    cout << "3. Apply Filters. " << endl;
+    cout << "4. Manual Editing."<< endl;
+    cout << "5. Export image. " << endl;
+    cout << "6. Reports. "<< endl;
+    cin >> seleccion;
+
+    switch(seleccion)
+    {
+    }
+
+
+    return 0;
+}
