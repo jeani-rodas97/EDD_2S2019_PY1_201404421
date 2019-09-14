e3e45cb67259771a8df1fdc18ee2b0d5ab8e9f7a
jeani-rodas97 <jenni1897@gmail.com>
2019-09-13 19:00:57 -0600

    Nodo Inicio

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <sstream>  //Para usar streamstring
 #include <string>   //Para reconocer string
 #include <stdlib.h> //Para limpiar la consola
-#include <ListaInicialCabecera.h>
 #include <NodoConfig.h>
 
 using namespace std;
@@ -19,11 +18,10 @@ struct MiArchivo
 int main()
 {
     MiArchivo inicio;
-    ListaInicialCabecera ListaInicial;
     int seleccion;
     int NumCapa; string NameArchivo; //Archivo Inicial
     int Ancho, Alto, PixAncho, PixAlto; //Configuration
-    string Archivo, ruta;
+    string Archivo;
     cout << "%%%%%%%%%%%%%%%%%%%% MENU %%%%%%%%%%%%%%%%%%%%"<< endl;
     cout << "1. Insert image. "<< endl;
     cout << "2. Select -image. "<< endl;
@@ -36,30 +34,24 @@ int main()
     switch(seleccion)
     {
     case 1:
-        char nombre[20];
         cout << "%%%%%%%%%%%%%%%%%%%% IMAGES %%%%%%%%%%%%%%%%%%%%"<< endl;
         cout << "Ingrese nombre del archivo .csv" << endl;
         cin >> Archivo;
         ifstream leer;
+        string Name;
         leer.open(Archivo.c_str(), ios::in);
         while(leer.good())
         {
             for(string linea; getline(leer,linea);)
             {
                 leer>>inicio.id>>inicio.simb>>inicio.nombre;
+                Name = inicio.nombre;
                 cout<<"id "<< inicio.id;
                 cout<<" name " << inicio.nombre << endl;
-                if (inicio.nombre == "config.csv")
+                if(Name == "config.csv")
                 {
-                    leer.open("config.csv", ios::in);
-                    while(leer.good())
-                    {
-                        for(string lineaConfig; getline(leer,lineaConfig);)
-                        {
-                            leer>>Ancho>>Alto>>PixAncho>>PixAlto;
-                            cout<<Ancho<<Alto<<PixAncho<<PixAlto;
-                        }
-                    }
+
+                    cout<<"Leer config"<<endl;
                 }
             }
         }
